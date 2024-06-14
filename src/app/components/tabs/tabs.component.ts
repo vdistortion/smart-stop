671dbb33a6e14998836a2f2f762fa2d363d23bb2
Валентин <astrotrain55@gmail.com>
2024-06-14 09:03:51 +0500

    app-tabs

---
+++ src/app/components/tabs/tabs.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './tabs.component.html',
+  styleUrl: './tabs.component.scss',
+})
+export class TabsComponent {
+  active: number = 0;
+  tabs: [number, string][] = [
+    [
+      12,
+      `автобусы №2, 4, 64, 83. маршрутки №123, 128, 22, 261, 3, 50, 54, 64, 78, 86. троллейбусы №1, 10, 17, 19, 2, 26, 8 до остановки "алое поле" (около 2 мин в пути)`,
+    ],
+    [
+      17,
+      `автобус №51. маршрутки №17, 35, 40, 48, 53, 68. троллейбусы №12, 7 до остановки "областная больница" (около 10 мин в пути)`,
+    ],
+  ];
+}
--- src/app/pages/map-page/map-page.component.ts
+++ src/app/pages/map-page/map-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouteShortComponent } from '../../components/route-short/route-short.component';
 import { SearchComponent } from '../../components/search/search.component';
+import { TabsComponent } from '../../components/tabs/tabs.component';
 import { MapComponent } from '../../components/map/map.component';
 
 @Component({
   selector: 'app-map-page',
   standalone: true,
-  imports: [RouteShortComponent, SearchComponent, MapComponent],
+  imports: [RouteShortComponent, SearchComponent, TabsComponent, MapComponent],
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.scss',
 })
