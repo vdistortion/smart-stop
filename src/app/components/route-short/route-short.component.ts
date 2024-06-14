962fdcd38d4b435a7e10a64d22560cf7f728781d
Валентин <astrotrain55@gmail.com>
2024-06-14 08:34:03 +0500

    small fixed

--- src/app/components/route-short/route-short.component.ts
+++ src/app/components/route-short/route-short.component.ts
@@ -3,6 +3,7 @@ import { routeList } from '../../data/routes';
 import { RouterLink } from '@angular/router';
 import { ButtonRouteComponent } from '../button-route/button-route.component';
 import { EmptyComponent } from '../empty/empty.component';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-route-short',
@@ -13,4 +14,8 @@ import { EmptyComponent } from '../empty/empty.component';
 })
 export class RouteShortComponent {
   public routeList = routeList;
+
+  get routes() {
+    return this.routeList.slice(0, environment.COUNT_ROUTE_SHORT_LIST);
+  }
 }
