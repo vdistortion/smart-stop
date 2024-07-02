bd7edf54b4cf03c6f675858b3de6d7e8161abcb1
Валентин <astrotrain55@gmail.com>
2024-07-02 12:29:59 +0500

    rm debug

--- src/app/dev/debug/debug.component.ts
+++ src/app/dev/debug/debug.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'dev-debug',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
   templateUrl: './debug.component.html',
   styleUrl: './debug.component.scss',
 })
-export class DebugComponent {}
+export class DebugComponent {
+  protected show: boolean = environment.DEBUG;
+}
